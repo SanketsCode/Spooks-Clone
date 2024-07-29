14e3ec1b3b8b3f78ca0adba899cf5d1bae7fdcf1
Programmer Sanket <55836873+DevSanket@users.noreply.github.com>
2024-07-29 13:50:45 +0530

    FEATURE : Phone Call Button

--- src/components/PopupWidget.tsx
+++ src/components/PopupWidget.tsx
@@ -24,82 +24,55 @@ export function PopupWidget() {
 
   const userName = useWatch({ control, name: "name", defaultValue: "Someone" });
 
+  // Define the phone number
+  const phoneNumber = "+91 9922620768"; // Replace with the desired phone number
+
+  // Function to handle click and redirect to dialer
+  const handleClick = () => {
+    window.location.href = `tel:${phoneNumber}`;
+  };
+
   return (
     <div>
       <Disclosure>
-        {({ open }) => (
-          <>
-            <DisclosureButton className="fixed z-40 flex items-center justify-center transition duration-300 bg-indigo-500 rounded-full shadow-lg right-5 bottom-5 w-14 h-14 focus:outline-none hover:bg-indigo-600 focus:bg-indigo-600 ease">
-              <span className="sr-only">Open Contact form Widget</span>
-              <Transition
-                show={!open}
-                enter="transition duration-200 transform ease"
-                enterFrom="opacity-0 -rotate-45 scale-75"
-                leave="transition duration-100 transform ease"
-                leaveTo="opacity-0 -rotate-45"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="absolute w-6 h-6 text-white"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-                </svg>
-              </Transition>
-
-              <Transition
-                show={open}
-                enter="transition duration-200 transform ease"
-                enterFrom="opacity-0 rotate-45 scale-75"
-                leave="transition duration-100 transform ease"
-                leaveTo="opacity-0 rotate-45"
-                className="absolute w-6 h-6 text-white"
-                as={"div"}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <line x1="18" y1="6" x2="6" y2="18"></line>
-                  <line x1="6" y1="6" x2="18" y2="18"></line>
-                </svg>
-              </Transition>
-            </DisclosureButton>
-            <Transition
-              className="fixed  z-50 bottom-[100px] top-0 right-0  left-0 sm:top-auto sm:right-5 sm:left-auto"
-              enter="transition duration-200 transform ease"
-              enterFrom="opacity-0 translate-y-5"
-              leave="transition duration-200 transform ease"
-              leaveTo="opacity-0 translate-y-5"
-              as="div"
-            >
-              <DisclosurePanel className=" flex flex-col  overflow-hidden left-0 h-full w-full sm:w-[350px] min-h-[250px] sm:h-[600px] border border-gray-300 dark:border-gray-800 bg-white shadow-2xl rounded-md sm:max-h-[calc(100vh-120px)]">
-                <div className="flex flex-col items-center justify-center h-32 p-5 bg-indigo-600">
-                  <h3 className="text-lg text-white">How can we help?</h3>
-                  <p className="text-white opacity-50">
-                    Contact us with many options
-                  </p>
-                </div>
-                <div className="flex-grow h-full p-6 overflow-auto bg-gray-50 "></div>
-              </DisclosurePanel>
-            </Transition>
-          </>
-        )}
+        <DisclosureButton
+          onClick={handleClick}
+          className="fixed z-40 flex items-center justify-center transition duration-300 bg-indigo-500 rounded-full shadow-lg right-5 bottom-5 w-14 h-14 focus:outline-none hover:bg-indigo-600 focus:bg-indigo-600 ease"
+        >
+          <svg
+            height="32"
+            width="32"
+            color="#fff"
+            viewBox="0 0 256 256"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect fill="none" height="25" width="25" color="#fff" />
+            <path
+              d="M159.4,40A80.1,80.1,0,0,1,216,96.6"
+              fill="none"
+              stroke="#fff"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="12"
+            />
+            <path
+              d="M151.1,70.9a47.9,47.9,0,0,1,34,34"
+              fill="none"
+              stroke="#fff"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="12"
+            />
+            <path
+              d="M92.5,124.8a83.6,83.6,0,0,0,39,38.9,8,8,0,0,0,7.9-.6l25-16.7a7.9,7.9,0,0,1,7.6-.7l46.8,20.1a7.9,7.9,0,0,1,4.8,8.3A48,48,0,0,1,176,216,136,136,0,0,1,40,80,48,48,0,0,1,81.9,32.4a7.9,7.9,0,0,1,8.3,4.8l20.1,46.9a8,8,0,0,1-.6,7.5L93,117A8,8,0,0,0,92.5,124.8Z"
+              fill="none"
+              stroke="#fff"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="12"
+            />
+          </svg>
+        </DisclosureButton>
       </Disclosure>
     </div>
   );
